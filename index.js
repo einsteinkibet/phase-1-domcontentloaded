9a4f9c85d96b4bfbca0db08af2f402c15ae6010f
einsteinkibet <einsteinkibet0110@gmail.com>
2023-10-02 00:29:56 +0300

    completed ass

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the element with the specified ID
+    const textElement = document.getElementById('text');
+  
+    // Change the text content
+    textElement.textContent = 'This is really cool!';
+  });
